83f7cc7b916f327e5083e0c2eb3cc4939db5aed8
AMADEU TORQUATO XAVIER JUNIOR <ATXJUNI@embraer.com.br>
2022-10-26 10:42:17 -0300

    primeiro commit

---
+++ Js/script.js
@@ -0,0 +1,39 @@
+
+const calcular = document.getElementById('calcular');
+
+
+function imc () {
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
+
+    if (nome !== '' && altura !== '' && peso !== '') {
+
+        const valorIMC = (peso / (altura * altura)).toFixed(2);
+
+        let classificacao = '';
+
+        if (valorIMC < 18.5){
+            classificacao = 'Abaixo do peso.';
+        }else if (valorIMC < 25) {
+            classificacao = 'Com peso ideal. Parabéns!!!';
+        }else if (valorIMC < 30){
+            classificacao = 'Levemente acima do peso.';
+        }else if (valorIMC < 35){
+            classificacao = 'Com obesidade grau I.';
+        }else if (valorIMC < 40){
+            classificacao = 'Com obesidade grau II';
+        }else {
+            classificacao = 'Com obesidade grau III. Cuidado!!';
+        }
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+        
+    }else {
+        resultado.textContent = 'Preencha todos os campos!';
+    }
+
+}
+
+calcular.addEventListener('click', imc);
